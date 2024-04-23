1c6b562a9efa1a37782e241c57d3517c7630f369
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-04-23 15:18:45 +0300

    Add files via upload

---
+++ Matrix transposition/Matrix transposition/Matrix transposition.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// Функция для транспонирования матрицы
+vector<vector<int>> transpose(const vector<vector<int>>& matrix)
+{
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    // Создаем новую матрицу с перевернутыми размерами
+    vector<vector<int>> transposed(cols, vector<int>(rows));
+
+
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+
+    return transposed;
+}
+
+int main()
+{
+
+    vector<vector<int>> matrix = { {1, 2, 3}, {4, 5, 6} };
+    vector<vector<int>> transposedMatrix = transpose(matrix);
+
+    for (const auto& row : transposedMatrix)
+    {
+        for (int element : row)
+        {
+            cout << element << " ";
+        }
+        cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ names and phone numbers/names and phone numbers/names and phone numbers.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    vector<string> contacts(2);
+
+
+    cout << "Введите имя: ";
+    getline(cin, contacts[0]);
+
+    cout << "Введите телефон: ";
+    getline(cin, contacts[1]);
+
+    cout << "Контакт сохранен:" << endl;
+    cout << "Имя: " << contacts[0] << endl;
+    cout << "Телефон: " << contacts[1] << endl;
+
+    string searchName;
+    cout << "Введите имя для поиска: ";
+    getline(cin, searchName);
+
+    if (searchName == contacts[0])
+    {
+        cout << "Найден контакт:" << endl;
+        cout << "Имя: " << contacts[0] << endl;
+        cout << "Телефон: " << contacts[1] << endl;
+    }
+    else
+    {
+        cout << "Контакт с таким именем не найден." << endl;
+    }
+
+    string searchPhone;
+    cout << "Введите номер телефона для поиска: ";
+    getline(cin, searchPhone);
+
+    if (searchPhone == contacts[1])
+    {
+        cout << "Найден контакт:" << endl;
+        cout << "Имя: " << contacts[0] << endl;
+        cout << "Телефон: " << contacts[1] << endl;
+    }
+    else
+    {
+        cout << "Контакт с таким номером телефона не найден." << endl;
+    }
+
+    return 0;
+}
